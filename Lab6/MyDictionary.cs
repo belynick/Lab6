9fe5db9c0408a4b97770ccc14249b7d5a2fb5861
belynick <nikkill338@gmail.com>
2022-10-30 16:43:13 +0200

    Добавьте файлы проекта.

---
+++ Lab6/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+    class MyDictionary<Tkey, Tval>
+    {
+        private int counter = 0;
+        private Tkey[] keysArray = null;
+        private Tval[] valsArray = null;
+
+        public int Counter
+        {
+            get { return this.counter; }
+        }
+
+        public void Add(Tkey key, Tval val)
+        {
+            this.counter++;
+
+            Array.Resize(ref keysArray, counter);
+            keysArray[counter - 1] = key;
+
+            Array.Resize(ref valsArray, counter);
+            valsArray[counter - 1] = val;
+        }
+
+        public Tval this[Tkey key]
+        {
+            get
+            {
+                int ind = 0;
+                for (int i = 0; i < keysArray.Length; i++)
+                {
+                    if (key.Equals(keysArray[i]))
+                        ind = i;
+                }
+                return valsArray[ind];
+            }
+        }
+
+
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+
+            foreach (int item in list.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(MyClass<List<int>>.FactoryMethod());
+        }
+    }
+}
